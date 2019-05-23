7cae2fc8aa8354cffa0a19ddb106cb84fdc1bee2
ojathelonius <louis.johanet@epfedu.fr>
2019-05-24 00:11:53 +0200

    move api functions to api.js

---
+++ app/src/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const base_url = 'http://localhost:5000';
+
+export function get(path) {
+    try {
+        return axios.get(base_url + path);
+    } catch (error) {
+        console.error(`Error fetching resource at ${path} : ${error}`);
+    }
+}
--- app/src/functions.js
+++ app/src/functions.js
@@ -6,33 +6,29 @@ import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import XYZ from 'ol/source/XYZ';
 import GeoJSON from 'ol/format/GeoJSON';
-import axios from 'axios';
 import {
     Vector as VectorSource
 } from 'ol/source';
 import {
     Vector as VectorLayer
 } from 'ol/layer';
+import {
+    get
+} from './api';
 
 /**
  * Update offers
  * @param {ol/Map} map 
  */
 export async function updateOffers(map) {
-    try {
-        const response = await axios.get('http://localhost:5000/offers');
-        var vectorSource = new VectorSource({
-            features: (new GeoJSON()).readFeatures(response.data)
-        });
-
-        var vectorLayer = new VectorLayer({
-            source: vectorSource
-        });
-
-        map.addLayer(vectorLayer);
-    } catch (error) {
-        console.error(error);
-    }
+    const response = await get('/offers');
+    var vectorSource = new VectorSource({
+        features: (new GeoJSON()).readFeatures(response.data)
+    });
+    var vectorLayer = new VectorLayer({
+        source: vectorSource
+    });
+    map.addLayer(vectorLayer);
 }
 
 /**

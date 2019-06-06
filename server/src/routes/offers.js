bfff404b9a8752eaec4cd9f558bc2be954665cbe
ojathelonius <louis.johanet@epfedu.fr>
2019-06-06 22:49:49 +0200

    OR -> AND in offers query

--- server/src/routes/offers.js
+++ server/src/routes/offers.js
@@ -12,34 +12,36 @@ export default () => {
         let params = req.query.params && req.query.params.split(',').map(element => element.toLowerCase());
 
         // Dataset size allows not using full text search from Postgres
+        // Note : OR request would be the same, except with :
+        // `AND LOWER(description) SIMILAR TO '%(' || ${params.map((element, index) => `$${(index+1)}`).join(` || '|' || `)} || ')%'`
         const result = await pg_client.query(`
+            SELECT
+                'FeatureCollection' AS "type",
+                array_to_json(array_agg(f)) AS "features"
+            FROM (
                 SELECT
-                    'FeatureCollection' AS "type",
-                    array_to_json(array_agg(f)) AS "features"
-                FROM (
-                    SELECT
-                        'Feature' AS "type",
-                        ST_ASGeoJSON(geom)::json AS "geometry",
-                        (
-                            SELECT row_to_json(t)
-                            FROM (
-                                SELECT
-                                    position,
-                                    company,
-                                    country,
-                                    city,
-                                    salary,
-                                    civiweb_id
-                            ) t
-                        ) AS "properties"
-                    FROM offer
-                    WHERE
-                        (lat IS NOT null
-                        OR lon IS NOT null)
-                    ${params ? 
-                        `AND LOWER(description) SIMILAR TO '%(' || ${params.map((element, index) => `$${(index+1)}`).join(` || '|' || `)} || ')%'`
-                    : ''}
-                ) AS f
+                    'Feature' AS "type",
+                    ST_ASGeoJSON(geom)::json AS "geometry",
+                    (
+                        SELECT row_to_json(t)
+                        FROM (
+                            SELECT
+                                position,
+                                company,
+                                country,
+                                city,
+                                salary,
+                                civiweb_id
+                        ) t
+                    ) AS "properties"
+                FROM offer
+                WHERE
+                    (lat IS NOT null
+                    OR lon IS NOT null)
+                ${params ? 
+                    `AND LOWER(description) LIKE ALL(array[${params.map((element, index) => `'%' || $${(index+1)} || '%'`).join(',')}])`
+                : ''}
+            ) AS f
         `, params);
         if(result.rows[0].features == null) {
             result.rows[0].features = [];
